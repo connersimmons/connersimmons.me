18f2b36444b487d1132ba9c99628d31053b110a8
connersimmons <conner.simmons@scranton.edu>
2015-09-18 23:51:21 -0400

    service.php change

--- thesis/service.php
+++ thesis/service.php
@@ -1,37 +1,44 @@
 <?php
+/* connect to the db */
+$link = mysql_connect('localhost','connersimmons','brownie4') or die('Cannot connect to the DB');
+mysql_select_db('bbmobile',$link) or die('Cannot select the DB');
 
-// Create connection
-$con=mysqli_connect("localhost","connersimmons","brownie4","bbmobile");
+/* grab the posts from the db */
+$query = "SELECT * FROM vendor ORDER BY firstname";
+$result = mysql_query($query,$link) or die('Errant query:  '.$query);
 
-// Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+/* create one master array of the records */
+$posts = array();
+if(mysql_num_rows($result)) {
+  while($post = mysql_fetch_assoc($result)) {
+    $posts[] = array('post'=>$post);
+  }
 }
 
-// This SQL statement selects ALL from the table 'Locations'
-$sql = "SELECT * FROM Locations";
-
-// Check if there are results
-if ($result = mysqli_query($con, $sql))
-{
-	// If so, then create a results array and a temporary one
-	// to hold the data
-	$resultArray = array();
-	$tempArray = array();
-
-	// Loop through each row in the result set
-	while($row = $result->fetch_object())
-	{
-		// Add each row into our results array
-		$tempArray = $row;
-	    array_push($resultArray, $tempArray);
-	}
-
-	// Finally, encode the array to JSON and output the results
-	echo json_encode($resultArray);
+/* output in necessary format */
+if($format == 'json') {
+  header('Content-type: application/json');
+  echo json_encode(array('posts'=>$posts));
+}
+else {
+  header('Content-type: text/xml');
+  echo '<posts>';
+  foreach($posts as $index => $post) {
+    if(is_array($post)) {
+      foreach($post as $key => $value) {
+        echo '<',$key,'>';
+        if(is_array($value)) {
+          foreach($value as $tag => $val) {
+            echo '<',$tag,'>',htmlentities($val),'</',$tag,'>';
+          }
+        }
+        echo '</',$key,'>';
+      }
+    }
+  }
+  echo '</posts>';
 }
 
-// Close connections
-mysqli_close($con);
+/* disconnect from the db */
+@mysql_close($link);
 ?>
